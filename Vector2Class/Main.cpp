86b8f40fde25c9423180397bd18675581720eeaa
TaureanRoberts <taureanjroberts@gmail.com>
2017-11-06 16:18:55 -0600

    .

--- Vector2Class/Main.cpp
+++ Vector2Class/Main.cpp
@@ -10,7 +10,15 @@ using namespace std;
 
 int main()
 {
+	Vector2 test2;
 	Vector3 test1;
+	Vector4 test3;
+
+	cin >> test3;
+	cout << test3;
+
+	cin >> test2;
+	cout << test2;
 
 	cin >> test1;
 	cout << test1;
