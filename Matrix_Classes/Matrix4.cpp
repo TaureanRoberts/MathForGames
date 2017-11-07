2850c428788d154e44751caee733fc58d9784202
Taurean Roberts <taureanjroberts@gmail.com>
2017-11-06 21:45:36 -0600

    Matrix library

--- Matrix_Classes/Matrix4.cpp
+++ Matrix_Classes/Matrix4.cpp
@@ -1,3 +1,41 @@
 #include <iostream>
 #include "Matrix4.h"
 #include <math.h>
+
+Matrix4::Matrix4()
+{
+}
+
+Matrix4::Matrix4(float mat[])
+{
+	mMat[12] = mat[12];
+}
+
+Matrix4::Matrix4(float indexA)
+{
+}
+
+Matrix4 Matrix4::operator+(Matrix4 & other)
+{
+	return Matrix4();
+}
+
+Matrix4 Matrix4::operator-(Matrix4 & other)
+{
+	return Matrix4();
+}
+
+Matrix4 Matrix4::operator*(Matrix4 & other)
+{
+	return Matrix4();
+}
+
+ostream & operator<<(ostream & output, Matrix4 & other)
+{
+	// TODO: insert return statement here
+}
+
+istream & operator>>(istream & input, Matrix4 & other)
+{
+	// TODO: insert return statement here
+}
--- Matrix_Classes/Matrix4.h
+++ Matrix_Classes/Matrix4.h
@@ -1,3 +1,17 @@
 #pragma once
 #include <iostream>
 using namespace std;
+
+class Matrix4
+{
+public:
+	float mMat[];
+	Matrix4();
+	Matrix4(float mat[]);
+	Matrix4(float indexA);
+	Matrix4 operator +(Matrix4 &other);
+	Matrix4 operator -(Matrix4 &other);
+	Matrix4 operator *(Matrix4 &other);
+	friend ostream& operator << (ostream &output, Matrix4 &other);
+	friend istream& operator >> (istream &input, Matrix4 &other);
+};
