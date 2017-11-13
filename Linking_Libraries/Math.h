e610b1b529173dbca2c9f7d94eb8f5dbd5d1f7fd
TaureanRoberts <taureanjroberts@gmail.com>
2017-11-13 17:57:41 -0600

    In Class Work

---
+++ Linking_Libraries/Math.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+
+class Math
+{
+public:
+	float Add(float lhs, float rhs);
+	float Subtract(float lhs, float rhs);
+	float Multiply(float lhs, float rhs);
+	float Divide(float lhs, float rhs);
+};
