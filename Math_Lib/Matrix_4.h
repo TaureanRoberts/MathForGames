91c6e799f07fcac31a2a42afec9b14dc76360f72
Taurean Roberts <taureanjroberts@gmail.com>
2017-12-13 10:50:32 -0600

    Documentation

--- Math_Lib/Matrix_4.h
+++ Math_Lib/Matrix_4.h
@@ -4,20 +4,64 @@ using namespace std;
 class Matrix4
 {
 public:
+	//Prototype: float mMat[16]
+	//Description: An Array of size 16
+	//Precondition: Taken in as a float to be assignesd for the matrix
+	//PostCondition: Sets size of the array for the class
+	//Protection Level: public
+	float mMat[16];
+	//Prototype: Matrix4()
+	//Description: Default construct
+	//Precondition: n/a
+	//PostCondition: n/a
+	//Protection Level: public
+	Matrix4();
+	//Prototype: Matrix4(float mat[])
+	//Description: Default that takes a float array with no set size
+	//Precondition:
+	//PostCondition:
+	//Protection Level: public
+	Matrix4(float mat[]);
+	//Prototype: Matrix4(float indexA, float indexB, float indexC, float indexD, float indexE, float indexF, float indexG, float indexH, float indexI, float indexJ, float indexK, float indexL, float indexM, float indexN, float indexO, float indexP);
+	//Description:
+	//Precondition:
+	//PostCondition:
+	//Protection Level: public
+	Matrix4(float indexA, float indexB, float indexC, float indexD, float indexE, float indexF, float indexG, float indexH, float indexI, float indexJ, float indexK, float indexL, float indexM, float indexN, float indexO, float indexP);
+	//Prototype: Matrix4 operator +(Matrix4 &other);
+	//Description:
+	//Precondition:
+	//PostCondition:
+	//Protection Level: public
+	Matrix4 operator +(Matrix4 &other);
 	//Prototype:
 	//Description:
 	//Precondition:
 	//PostCondition:
 	//Protection Level:
-	float mMat[16];
-
-	Matrix4();
-	Matrix4(float mat[]);
-	Matrix4(float indexA, float indexB, float indexC, float indexD, float indexE, float indexF, float indexG, float indexH, float indexI, float indexJ, float indexK, float indexL, float indexM, float indexN, float indexO, float indexP);
-	Matrix4 operator +(Matrix4 &other);
 	Matrix4 operator -(Matrix4 &other);
+	//Prototype:
+	//Description:
+	//Precondition:
+	//PostCondition:
+	//Protection Level:
 	Matrix4 operator *(Matrix4 &other);
+	//Prototype:
+	//Description:
+	//Precondition:
+	//PostCondition:
+	//Protection Level:
 	bool operator == (Matrix4 &other);
+	//Prototype:
+	//Description:
+	//Precondition:
+	//PostCondition:
+	//Protection Level:
 	friend ostream& operator << (ostream &output, Matrix4 &other);
+	//Prototype:
+	//Description:
+	//Precondition:
+	//PostCondition:
+	//Protection Level:
 	friend istream& operator >> (istream &input, Matrix4 &other);
 };
