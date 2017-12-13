87583d5ff1fc1d34ef93df0c40e07f02c9d4c46d
Taurean Roberts <taureanjroberts@gmail.com>
2017-12-13 10:07:24 -0600

    Matrix3 documentation

--- Math_Lib/Matrix_3.h
+++ Math_Lib/Matrix_3.h
@@ -5,10 +5,10 @@ using namespace std;
 class Matrix3
 {
 public:
-	//Prototype: 	float mMat[9];
-	//Description:
-	//Precondition:
-	//Post Condition:
+	//Prototype: float mMat[9];
+	//Description: Array with the size of 9
+	//Precondition: Used to assign the array of size 9 when called
+	//Post Condition: Allows placment within the set array range
 	//Protection Level: public
 	float mMat[9];
 	//Prototype:Matrix3();
@@ -36,21 +36,21 @@ public:
 	//Protection Level: public
 	Matrix3 operator +(Matrix3 &other);
 	//Prototype:	Matrix3 operator -(Matrix3 &other);
-	//Description: subtracts a 
+	//Description: subtracts one side from another
 	//Precondition: takes in a arrry 
  	//Post Condition: returns a new array
 	//Protection Level: public
 	Matrix3 operator -(Matrix3 &other);
 	//Prototype: Matrix3 operator *(Matrix3 &other);
-	//Description: scales the array
+	//Description: scales the array to a number
 	//Precondition: takes the original array 
 	//Post Condition: scales it by the set size
 	//Protection Level: public
 	Matrix3 operator *(Matrix3 &other);
 	//Prototype: bool operator == (Matrix3 &other);
-	//Description: compares the strings 
-	//Precondition: 
-	//Post Condition:
+	//Description: Compares the strings 
+	//Precondition: Compares the two values and variables and connects the two.
+	//Post Condition: Checks if the two are the same or the condition matches the rules of said function
 	//Protection Level: public
 	bool operator == (Matrix3 &other);
 	//Prototype: friend ostream& operator << (ostream &output, Matrix3 &other);
